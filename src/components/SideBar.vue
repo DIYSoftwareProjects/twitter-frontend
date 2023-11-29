b49be8a168429ce1989bff9d5dd5f12d199c4de3
dwinanto34 <87220725+dwinanto34@users.noreply.github.com>
2023-11-30 00:23:01 +0900

    Logout feature

--- src/components/SideBar.vue
+++ src/components/SideBar.vue
@@ -18,6 +18,40 @@
           <span class="sidebar-text">Profile</span>
         </router-link>
       </div>
+
+      <div class="sidebar-link">
+        <div class="sidebar-link-router" @click="logout">
+          <i class="fa-solid fa-right-from-bracket sidebar-icon"></i>
+          <span class="sidebar-text">Logout</span>
+        </div>
+      </div>
+
     </div>
   </div>
 </template>
+
+<script>
+import { getAuth, signOut } from "firebase/auth"
+import VueCookies from 'vue-cookies'
+
+export default {
+  methods: {
+    async logout() {
+      try {
+        const auth = getAuth()
+        signOut(auth).then(() => {
+          VueCookies.remove('bearerToken')
+          VueCookies.remove('userId')
+          VueCookies.remove('isAuthenticated')
+        }).catch((error) => {
+          window.alert(error)
+        })
+        this.$router.push('/login')
+      } catch (error) {
+        window.alert(error)
+      }
+    }
+  }
+}
+
+</script>
